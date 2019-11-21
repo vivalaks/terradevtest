ddcd63f437c08da41da270900cb4a2cd246ca9ce
Lakshmana Kattula <vivalaks@gmail.com>
2019-11-21 20:27:18 +0000

    First example with GCP. Created a VPC. Example from Hashicorp learn Getting Started GCP.

--- google/main.tf
+++ google/main.tf
@@ -1,2 +1,11 @@
 provider "google" {
+	credentials = file("/Users/laks/Downloads/laks-proj1-appeng-t1-208fab3fde44.json")
+	project = "laks-proj1-appeng-t1"
+	region  = "us-central1"
+	zone    = "us-central1-c"
 }
+
+resource "google_compute_network" "vpc_network" {
+	name = "terraform-network"
+}
+
